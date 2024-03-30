66a4008fa9fe77ec58e771233884ecdb6d5b3eb4
unknown <nitishrawat009@gmail.com>
2024-03-30 23:16:56 +0530

    solved problem 2

--- weekly-contests/119/problem2.cpp
+++ weekly-contests/119/problem2.cpp
@@ -1,21 +1,27 @@
 int numberOfElement(int n, vector<int> &v) {
     // Write your code here.
-    int counter = 0;
-    for(int i = 0; i < n; i++){
 
-      int prev;  
-      if (i-1 < 0){ prev = 0; }
-      else{ prev = v[i-1]; }
+      int counter = 0;
+      for(int i = 0; i < n; i++){
 
-      int next;
-      if(i+1 > (n-1)){ next = 0; }
-      else{ next = v[i+1]; }
-      
-      int sum = prev+v[i]+next;
-      if( ((sum/2)==v[i]) || (((sum+1)/2)==v[i]) ){
+        int prev;  
+        if (i-1 < 0){ prev = 0; }
+        else{ prev = v[i-1]; }
+
+        int next;
+        if(i+1 > (n-1)){ next = 0; }
+        else{ next = v[i+1]; }
+        
+        int a = v[i] - prev;
+        int b = next - v[i];
+        bool middle = false;
+        if( (a*b)>=0 ){
+          middle = true;
+        }
+        if (middle) {
           counter++;
+        }
       }
-    }
 
     return counter;
 }
